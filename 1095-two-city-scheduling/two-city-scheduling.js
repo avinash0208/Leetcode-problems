fda5480e2068cd70b7019467993f1a47caa504a8
avinash0208 <35329694+avinash0208@users.noreply.github.com>
2026-08-11 09:05:04 +0530

    Time: 0 ms (100.00%) | Memory: 52.5 MB (99.44%) - LeetSync

---
+++ 1095-two-city-scheduling/two-city-scheduling.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+var twoCitySchedCost = function(costs) {
+    costs.sort((a,b)=>((b[1]-b[0])-(a[1]-a[0])))
+    let total=0
+    let n= costs.length/2
+    for(let i=0;i<n;i++){
+        total+=costs[i][0]
+        total+=costs[i+n][1]
+    }
+    
+    return total
+};
