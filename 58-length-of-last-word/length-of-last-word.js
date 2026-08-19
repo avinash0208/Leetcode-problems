3a1edc3237f8bc2a17b6e690998e68830ed98a6c
avinash0208 <35329694+avinash0208@users.noreply.github.com>
2026-08-19 20:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 53.7 MB (53.41%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,12 +5,12 @@
 var lengthOfLastWord = function(s) {
     s= s.trim()
     let count =0
-    for(let i=s.length;i>=0;i--){
+    for(let i=s.length-1;i>=0;i--){
         if(s[i]===' '){
             break;
         }else{
             count+=1
         }
     }
-    return count-1
+    return count
 }
