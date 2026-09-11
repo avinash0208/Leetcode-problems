14653bed7d596352f964471d99cdf4fd09e08efc
avinash0208 <35329694+avinash0208@users.noreply.github.com>
2026-09-11 17:42:10 +0530

    Time: 2 ms (19.24%) | Memory: 63 MB (8.71%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -10,17 +10,27 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var maxDepth = function(root) {
-    let maxDepth=0
-    if(!root) return 0
-    const findDepth=(curr,depth)=>{
-       if(depth>maxDepth){
-            maxDepth=depth
-       }
-       curr.left && findDepth(curr.left, depth+1)
-       curr.right && findDepth(curr.right, depth+1)
 
-    }
-    findDepth(root,1)
-    return maxDepth
+// TOP DOWN Approach
+// var maxDepth = function(root) {
+//     let maxDepth=0
+//     if(!root) return 0
+//     const findDepth=(curr,depth)=>{
+//        if(depth>maxDepth){
+//             maxDepth=depth
+//        }
+//        curr.left && findDepth(curr.left, depth+1)
+//        curr.right && findDepth(curr.right, depth+1)
+
+//     }
+//     findDepth(root,1)
+//     return maxDepth
+// };
+
+// Bottom UP approach
+
+var maxDepth = function(root) {
+    if(!root) return 0
+    // MaxDepth at ith sub-tree = 1 + Max(depth at left subtree, depth at right subtree)
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
